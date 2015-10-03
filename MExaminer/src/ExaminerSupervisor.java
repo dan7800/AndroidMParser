04c7b2f29f82aeeaa8c2d33be6293bc7a14f4796
dan7800@yahoo.com <Dan>
2015-10-03 11:01:23 +0000

    initial test checkin

---
+++ MExaminer/src/ExaminerSupervisor.java
@@ -0,0 +1,19 @@
+
+public class ExaminerSupervisor {
+	
+	private final String apkLocation = "";
+
+
+	public static void main(String[] args) {
+		ExaminerSupervisor es = new ExaminerSupervisor();
+		es.Run();
+
+	}
+
+	
+	private void Run(){
+		System.out.println("hello world");
+	}
+	
+	
+}
