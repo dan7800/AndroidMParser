cb04d3b9bc040fd59a038f9673cdaaf61b6d14d0
Dan Krutz <dxkvse@rit.edu>
2016-02-26 10:43:26 -0500

    adding sqlite jar

--- CommitAnalyzer/src/CommitAnalyzerSuper.java
+++ CommitAnalyzer/src/CommitAnalyzerSuper.java
@@ -1,12 +1,36 @@
+import java.sql.*;
 
 public class CommitAnalyzerSuper {
 
-	/**
-	 * @param args
-	 */
+	private final String DbLocation = "E:\\Dropbox\\confpapers\\AndroidData\\F-Droid_LifeCycles\\data\\AndrosecData.sqlite";
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		CommitAnalyzerSuper cas = new CommitAnalyzerSuper();
+		//cas.Run();
+		cas.dbConnect();
 	}
 
-}
+	
+	private void Run(){
+		System.out.println("dan");
+	}
+	
+	
+	private void dbConnect(){
+		
+		Connection c = null;
+	    try {
+	      Class.forName("org.sqlite.JDBC");
+	      c = DriverManager.getConnection("jdbc:sqlite:test.db");
+	    } catch ( Exception e ) {
+	      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+	      System.exit(0);
+	    }
+	    System.out.println("Opened database successfully");
+	  }
+		
+		
+	}
+	
+	
+
