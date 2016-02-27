a7e7d920c5f10a3e2ea918df86f8f11f03ce31d8
dkrutz <dan@go.com>
2016-02-26 22:27:39 -0500

    nightly checkin

--- CommitAnalyzer/src/CommitAnalyzerSuper.java
+++ CommitAnalyzer/src/CommitAnalyzerSuper.java
@@ -1,3 +1,9 @@
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -7,14 +13,23 @@ public class CommitAnalyzerSuper {
 //	private final String DbLocation = "E:\\Dropbox\\confpapers\\AndroidData\\F-Droid_LifeCycles\\data\\AndrosecData.sqlite";
 	private final String DbLocation = "/Users/dxkvse/Dropbox/confpapers/AndroidData/F-Droid_LifeCycles/data/AndrosecData.sqlite";
 		
+	private int statementCount=0;
+	
+	
+	// create list of all dbInserts -- Prevents locking
+	private List insertStatements = new ArrayList();
+	
 	
 	public static void main(String[] args) {
 		
 		CommitAnalyzerSuper cas = new CommitAnalyzerSuper();
 		// PrepDB(); // Prepare the database for inserting values
 		cas.buildChangeList();
+		
+		
+		
+		
 	}
-
 	
 	
 	// Create the list of changes made to the DB
@@ -38,7 +53,7 @@ public class CommitAnalyzerSuper {
 		    	c.setAutoCommit(false);
 		
 		    	stmt = c.createStatement();
-		    	//String sqlAllApps="select * from ManifestPermissionCommitt_view";
+		  //  	String sqlAllApps="select * from ManifestPermissionCommitt_view";
 		    	final String sqlAllApps="select * from dummy";
 		    	ResultSet rsAllApps = stmt.executeQuery( sqlAllApps );
 		    	
@@ -47,7 +62,7 @@ public class CommitAnalyzerSuper {
 		    		 if(Integer.parseInt(rsAllApps.getString("appID"))!=AppID){
 
 		    			 if(AppID > 0){ // don't run the 1st time
-		    				 AnalyzeLists(permissionsList_Prev, permissionsList_Current);
+		    				 AnalyzeLists(permissionsList_Prev, permissionsList_Current, AppID, commitID);
 		    			 }
 		    			
 		    			 AppID=Integer.parseInt(rsAllApps.getString("appID"));
@@ -63,7 +78,7 @@ public class CommitAnalyzerSuper {
 		    			 if(Integer.parseInt(rsAllApps.getString("Commit_ID"))!=commitID){  // new commitID
 		    				 commitID=Integer.parseInt(rsAllApps.getString("commit_ID"));
 		    				 
-		    				 AnalyzeLists(permissionsList_Prev, permissionsList_Current);
+		    				 AnalyzeLists(permissionsList_Prev, permissionsList_Current, AppID, commitID);
 
 		    				 permissionsList_Prev.clear();
 		    				 permissionsList_Prev.addAll(permissionsList_Current); // setting them to be equal creates problems
@@ -79,13 +94,64 @@ public class CommitAnalyzerSuper {
 		   		 
 		    	 }
 	
-		    	 AnalyzeLists(permissionsList_Prev, permissionsList_Current);
-		    	 System.out.println(counter);
+		    	 AnalyzeLists(permissionsList_Prev, permissionsList_Current, AppID, commitID);
+		    	// System.out.println("Final count:" + counter);
 
 		   } catch ( Exception e ) {
 			      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
 			      System.exit(0);
 			    }
+	
+	
+		 
+		 for(int z=0; z<insertStatements.size(); z++){
+			// addManifestChange(insertStatements.get(z).toString());
+			// System.out.println(insertStatements.size());
+		 }
+		 
+			
+		System.out.println("Statement count " + statementCount);
+	}
+	
+	
+	
+	private void addManifestChange(String sql){
+		System.out.println("add change " + sql);
+		
+		
+		 PrintWriter out = null;
+		try {
+			out = new PrintWriter(new FileWriter("output.txt", true), true);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	      out.write(sql);
+	      out.close();
+		
+		
+		/*
+		Connection c = null;
+	    Statement stmt = null;
+		 try {
+		    	Class.forName("org.sqlite.JDBC");
+		      
+		    	c = DriverManager.getConnection("jdbc:sqlite:"+DbLocation);
+		    	c.setAutoCommit(false);
+		    	
+		    	stmt = c.createStatement();
+		       
+		        stmt.executeUpdate(sql);
+		        c.commit();
+		    	
+		    } catch ( Exception e ) {
+			      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+			      System.exit(0);
+			    }
+	
+		*/
+		
 	}
 	
 	
@@ -131,35 +197,72 @@ public class CommitAnalyzerSuper {
 	}
 	
 	
-	private void AnalyzeLists(List la, List lb){
-		System.out.println("**********analyze Lists********" + la.size() + " " + lb.size());
+	// make the necessary changes 
+	private void alterAndroid_Manifest_Commit_Changes(int appID, int commitID, int permissionID, String change){
+		//System.out.println(appID + " " +commitID+ " " + permissionID+ " " + change);
+		
+
+		String sql = "insert into Android_Manifest_Commit_Changes (AppID, CommitID, PermissionID, Action) values ("+appID+","+commitID+","+permissionID+",'"+change+"');";
+		insertStatements.add(sql);
+		
+		/*
+		Connection c = null;
+	    Statement stmt = null;
+		 try {
+		    	Class.forName("org.sqlite.JDBC");
+		      
+		    	c = DriverManager.getConnection("jdbc:sqlite:"+DbLocation);
+		    	c.setAutoCommit(false);
+		    	
+		    	
+		    	stmt = c.createStatement();
+		       // String sql = "delete from Android_Manifest_Commit_Changes";
+		    	String sql = "insert into Android_Manifest_Commit_Changes (AppID, CommitID, PermissionID, Action) values ("+appID+","+commitID+","+permissionID+",'"+change+"');";
+		        stmt.executeUpdate(sql);
+		        c.commit();
+		    	
+		    } catch ( Exception e ) {
+			      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+			      System.exit(0);
+			    }
+		
+		*/
+	}
+	
+	
+	private void AnalyzeLists(List la, List lb, int appID, int commitID){
+	//	System.out.println("**********analyze Lists********" + la.size() + " " + lb.size());
 		// get what was added
-		getAdded(la, lb);
-		getMissing(la, lb);
+		getAdded(la, lb, appID, commitID);
+		getMissing(la, lb, appID, commitID);
 			
 		// get what was removed
 	}
 
 	//Get what was added to the permissions list
-	private void getAdded(List la, List lb){
+	private void getAdded(List la, List lb, int appID, int commitID){
 		
 		// Loop through each of the items and then add them to the db
-		List added = getAddedListValues(la, lb);
+		List added = getAddedListValues(la, lb, appID, commitID);
 		for(int i=0; i<added.size(); i++){
-			System.out.println("Added:" + added.get(i));
+			System.out.println("Added:" + added.get(i) + " appID: " + appID + " CommitID:" + commitID);
+			alterAndroid_Manifest_Commit_Changes(appID, commitID, Integer.parseInt(added.get(i).toString()), "A");
+			statementCount = statementCount+1;
 		}
 	}
 
 	// Get what was removed from the list
-	private void getMissing(List la, List lb){
+	private void getMissing(List la, List lb, int appID, int commitID){
 		List removed = getMissingListValues(la, lb);
 		for(int i=0; i<removed.size(); i++){
-			System.out.println("Removed:" + removed.get(i));
+			System.out.println("appID: " + appID + " CommitID:" + commitID + " permissionID:" + );
+			alterAndroid_Manifest_Commit_Changes(appID, commitID, Integer.parseInt(removed.get(i).toString()), "R");
+			statementCount = statementCount+1;
 		}
 	}
 
 	// Create the list of added items
-	private List getAddedListValues(List la, List lb){
+	private List getAddedListValues(List la, List lb, int appID, int commitID){
 		List toReturn = new ArrayList(lb);
 		toReturn.removeAll(la);
 		return toReturn;
